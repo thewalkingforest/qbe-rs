3e8fd5c2ce8a672543d4aa46e37d102aa2fd2d26
prashantrahul141 <prashantrahul141@protonmail.com>
2025-04-11 10:00:56 +0530

    feat: add phi instruction
    Signed-off-by: prashantrahul141 <prashantrahul141@protonmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -332,6 +332,10 @@ pub enum Instr<'a> {
     /// Fetches the next argument from a variable argument list
     Vaarg(Type<'a>, Value),
 
+    // Phi instruction
+    /// Selects value based on the control flow path into a block.
+    Phi(String, Value, String, Value),
+
     // Program termination
     /// Terminates the program with an error
     Hlt,
@@ -422,6 +426,13 @@ impl fmt::Display for Instr<'_> {
             Self::Ultof(val) => write!(f, "ultof {}", val),
             Self::Vastart(val) => write!(f, "vastart {}", val),
             Self::Vaarg(ty, val) => write!(f, "vaarg{} {}", ty, val),
+            Self::Phi(label_1, val_if_label_1, label_2, val_if_label_2) => {
+                write!(
+                    f,
+                    "phi @{} {}, @{} {}",
+                    label_1, val_if_label_1, label_2, val_if_label_2
+                )
+            }
             Self::Hlt => write!(f, "hlt"),
         }
     }
